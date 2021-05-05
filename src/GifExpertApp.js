81d8abf7b7f2955968670ae2dd4184914640857f
juliomolinadev <jmolina.dex@gmail.com>
2021-05-04 21:30:41 -0700

    Sin estado inicial

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-	const [categories, setCategories] = useState(["Naruto"]);
+	const [categories, setCategories] = useState([""]);
 
 	return (
 		<>
